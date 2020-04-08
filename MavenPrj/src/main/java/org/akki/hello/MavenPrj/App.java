2a1a7187b249a9b8b3d5368835a71501c4c179c9
geesaladurgaakhil <durgaakhil0211@gmail.com>
2020-04-08 15:52:58 +0530

    1st commit

---
+++ MavenPrj/src/main/java/org/akki/hello/MavenPrj/App.java
@@ -0,0 +1,13 @@
+package org.akki.hello.MavenPrj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
